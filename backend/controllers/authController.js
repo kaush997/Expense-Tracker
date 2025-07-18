a7e2ae312f81a0b2e3ffeeeaadf5af1a026eae40
Kaushalya Hulathdoowage <skdilukshi97@gmail.com>
2025-07-18 20:03:05 +0530

    commit changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,3 +5,12 @@ const generateToken = (id) => {
     return jwt.sign({ id
     }, process.env.JWT_SECRET, { expiresIn: "1h"});
 }
+
+//Register User
+exports.registerUser = async (req, res) => {};
+
+//Register User
+exports.loginUser = async (req, res) => {};
+
+//Register User
+exports.getUserInfo = async (req, res) => {};
