ccaff55def50d3a092f2b3fb0304898eb7297fb2
Kaushalya Hulathdoowage <skdilukshi97@gmail.com>
2025-07-18 14:37:41 +0530

    commit backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const { connect } = require("http2");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Middleware to handle cors
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json());
+
+connectDB();
+
+const RORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -19,8 +19,28 @@ const SignUp = () => {
   const handleSignUp = async (e) => {
     e.preventDefault();
 
-    
-  }
+    let profileImageUrl = "";
+
+    if (!fullName){
+      setError("Please enter your name");
+      return;
+    }
+
+    if (!validateEmail(email)){
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password){
+      setError("Please enter the passsword");
+      return;
+    }
+
+    setError("");
+
+    //SignUp API Call
+    }
+
 
   return (
     <AuthLayout>
@@ -61,18 +81,18 @@ const SignUp = () => {
         </div>
           </div>
 
-          {error && <p className='text-red-500 text-xs pb-2.5 '>{error}</p>}
+        {error && <p className='text-red-500 text-xs pb-2.5 '>{error}</p>}
           
-                    <button type='submit' className='btn-primary '>
-                      SIGN UP
-                    </button>
+            <button type='submit' className='btn-primary '>
+              SIGN UP
+            </button>
           
-                    <p className='text-[13px] text-slate-800 mt-3'>
-                      Already have an account?{" "}
-                      <Link className="font-medium text-primary underline" to="/login">
-                        Login
-                      </Link>
-                    </p>
+              <p className='text-[13px] text-slate-800 mt-3'>
+                Already have an account?{" "}
+                  <Link className="font-medium text-primary underline" to="/login">
+                    Login
+                  </Link>
+              </p>
 
         </form>
       </div>
