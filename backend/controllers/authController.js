672732413526906df4bc5102a03dfaf4bfc78d9b
Kaushalya Hulathdoowage <skdilukshi97@gmail.com>
2025-07-18 16:15:17 +0530

    commit changes

---
+++ backend/controllers/authController.js
@@ -0,0 +1,7 @@
+const jwt = require("jsonwebtoken");
+
+//Generate JWT token
+const generateToken = (id) => {
+    return jwt.sign({ id
+    }, process.env.JWT_SECRET, { expiresIn: "1h"});
+}
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const { connect } = require("http2");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -20,5 +20,7 @@ app.use(express.json());
 
 connectDB();
 
-const RORT = process.env.PORT || 5000;
+app.use("/api/v1/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
