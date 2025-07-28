7d617f6013da66107225d06e494e4d7fcf38852f
Kaushalya Hulathdoowage <skdilukshi97@gmail.com>
2025-07-28 17:43:50 +0530

    commit changes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
 
 const app = express();
 
